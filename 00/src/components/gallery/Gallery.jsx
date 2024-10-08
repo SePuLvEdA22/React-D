d76ec1c7854f07d9caa7ea95fba8a8dbe722d582
Helger Santiago <114368002+SePuLvEdA22@users.noreply.github.com>
2024-10-08 08:21:11 -0500

    Update Gallery.jsx

--- 00/src/components/gallery/Gallery.jsx
+++ 00/src/components/gallery/Gallery.jsx
@@ -9,16 +9,21 @@ export default function Gallery() {
   //Estado para cofigurar el boton ver más.
   const [showMore, setShowMore] = useState(false);
 
-  // Devuelve true o false
-  const hasNext = index < sculptureList.length - 1;
+  // Devuelve true o false.
+  // sculptureList.length me retorna el numero total de elementos en el array.
+  // sculptureList.length - 1 me retorna el indice del ultimo elemento.
+  const hasNext = index < sculptureList.length - 1; // Verifica si el indice actual es menor que el ultimo elemento de la lista, si es asi devuelve true.
 
   /*
   Funcion usada en el boton de siguiente, nos ayuda a cambiar el indice, 
   por lo tanto cambiamos de objeto y de img.
   */
   function handleNextClick() {
+    // Si hasNext es true se suma uno al indice actual y se pasa al siguiente objeto.
     if (hasNext) {
       setIndex(index + 1);
+      // Si la respuesta es false volvemos al elemento con indice cero.
+      // hasNext devuelve false cuando el indice actual deja de ser menor que el ultimo indice del array. 
     } else {
       setIndex(0);
     }
@@ -29,14 +34,16 @@ export default function Gallery() {
   por lo tanto cambiamos de objeto y de img.
   */
   function handlePreviousClick() {
+    // Si devuelve true disminuimos en 1 el indice actual.
     if (index > 0) {
       setIndex(index - 1);
+      // Si el indice pasa a ser cero y queremos movernos hacia atras, nos enviara al ultimo elemento del array.
     } else {
-      sculptureList.length - 1;
+     setIndex(sculptureList.length - 1)
     }
   }
 
-  //Funcion usada en el boton de ver más
+  //Funcion usada en el boton de ver más.
   function handleMoreClick() {
     // ! Invierte el valor de showMore.
     setShowMore(!showMore);
@@ -46,6 +53,7 @@ export default function Gallery() {
 
   return (
     <>
+      {}
       <h2>Galeria de imagenes</h2>
       <button onClick={handlePreviousClick}>Anterior</button>
       <button onClick={handleNextClick}>Siguiente</button>
